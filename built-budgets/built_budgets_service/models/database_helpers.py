204b0b17b37bff900b37ea7b349e96be86246283
Sourcery AI <>
2023-04-25 14:31:40 +0000

    'Refactored by Sourcery'

--- built-budgets/built_budgets_service/models/database_helpers.py
+++ built-budgets/built_budgets_service/models/database_helpers.py
@@ -2,10 +2,4 @@ import os
 
 
 def get_connection_string():
-    return 'mysql+pymysql://%s:%s@%s:%s/%s' % (
-        os.environ['MYSQL_USER'],
-        os.environ['MYSQL_PASSWORD'],
-        os.environ['MYSQL_HOST'],
-        os.environ['MYSQL_PORT'],
-        os.environ['MYSQL_DATABASE'],
-    )
+    return f"mysql+pymysql://{os.environ['MYSQL_USER']}:{os.environ['MYSQL_PASSWORD']}@{os.environ['MYSQL_HOST']}:{os.environ['MYSQL_PORT']}/{os.environ['MYSQL_DATABASE']}"
